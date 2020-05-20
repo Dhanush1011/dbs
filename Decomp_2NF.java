19b9bf8cf91a3fdde1dd02175818e5733edb4411
Dhanush <karupakuladhanush@gmail.com>
2020-05-20 14:01:14 +0530

    Final raa shivudu

--- Decomp_2NF.java
+++ Decomp_2NF.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  *
@@ -18,9 +19,23 @@ import java.util.Set;
  */
 public class Decomp_2NF {
     
-    public void decomp2nf(HashMap<String, String> fds, String rls, Set<String> ckset)
+    public Set<String> decomp2nf(HashMap<String, String> fds, String rls, Set<String> ckset)
     {
+        Character[] rlsarr = ArrayUtils.toObject(rls.toCharArray());
+        Set<Character> rlsset = new HashSet<Character>(Arrays.asList(rlsarr));
+        
+        Set<Character> pset = new HashSet<Character>();
+        for(String x : ckset)
+        {
+            Character[] templ = ArrayUtils.toObject(x.toCharArray());
+            for(Character y : templ)
+                pset.add(y);
+            
+            
+        }
+        
         String x;
+        
         String temp = new String();
         String temps = new String();
         
@@ -37,57 +52,85 @@ public class Decomp_2NF {
             {
                 if(checksubstring(ck,x))
                 {
+                    Set<Character> tempset = new HashSet<Character>(Arrays.asList(ArrayUtils.toObject(entry.getValue().toCharArray())));
+                    for(Character c : tempset)
+                    {
+                        if(!pset.contains(c))
+                            rlsset.remove(c);
+                    }
                     relations.add(x.concat(entry.getValue()));
                     flag[count] = 1;
                     break;
                 }
             }
-            count++;
-            
+            count++;            
         }
-        for(String ck : ckset)
-        {
-            int f = 0;
-            for(String st : relations)
-            {
-                if(checksubstring(st,ck))
-                {
-                    f = 1;
-                    break;
-                }
-            }
-            if(f==0)
-                relations.add(ck);
-        }
+        
         count = 0;
         for(Map.Entry<String, String> entry : fds.entrySet())
         {
             x = entry.getKey();
-            if(flag[count]==0)
+            int flagck = 0;
+            for(String ck : ckset)
             {
-                System.out.println(entry.getValue());
+                if(checkstring(x,ck))
+                    flagck = 1;
+            }
+            
+            if(flagck==0 && flag[count]==0)
+            {
+                String tempval = "";
+                
                 for (String s : relations) {
-                    if(checksubstring(s,x))
+                    if(checkstring(s,x))
                     {
                         temps = new String(s);
                         temp = new String(s);
                         for(char c : entry.getValue().toCharArray())
                         {
                             if(temp.indexOf(c)==-1)
+                            {
                                 temp = temp + c;
+                                tempval = tempval + c;
+                        
+                            }
                         }
                         break;
                     }                    
                 }
                 
-                relations.remove(temps);
-                relations.add(temp);
+                if(!temp.isEmpty())                    
+                {
+                    Set<Character> tempset = new HashSet<Character>(Arrays.asList(ArrayUtils.toObject(tempval.toCharArray())));
+                    for(Character c : tempset)
+                    {
+                        if(!pset.contains(c))
+                            rlsset.remove(c);
+                    }
+                    System.out.println(rlsset.toString());
+                    relations.remove(temps);
+                    relations.add(temp);
+                }
             }
             count++;
         }
-              
         
+        
+        Character[] rfin = new Character[rlsset.size()];
+        rlsset.toArray(rfin);
+        String rfinnew = new String(ArrayUtils.toPrimitive(rfin));
+        relations.add(rfinnew);
         System.out.println(relations.toString());
+        return relations;
+    }
+    
+    public boolean sameChars(String firstStr, String secondStr) 
+    {
+        char[] first = firstStr.toCharArray();
+        char[] second = secondStr.toCharArray();
+        Arrays.sort(first);
+        Arrays.sort(second);
+        return Arrays.equals(first, second);
     }
     
     public boolean checksubstring(String s1, String s2)
@@ -102,7 +145,21 @@ public class Decomp_2NF {
                 flag = 1;
             }
         }
-        return (flag==0);
+        return (flag==0 && s1.length()>s2.length());
     }
     
+    public boolean checkstring(String s1, String s2)
+    {
+        char[] first = s1.toCharArray();
+        char[] second = s2.toCharArray();
+        int flag = 0;
+        for(char c : second)
+        {
+            if(s1.indexOf(c)==-1)
+            {
+                flag = 1;
+            }
+        }
+        return (flag==0 );
+    }
 }
